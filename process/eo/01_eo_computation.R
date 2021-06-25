48f036b144ae76f9fcfa3fac63a66c88369a90cc
adrhuerta <adrhuerta@gmail.com>
2021-06-25 01:36:11 -0500

    eo computation v1

---
+++ process/eo/01_eo_computation.R
@@ -0,0 +1,53 @@
+rm(list = ls())
+
+reticulate::use_virtualenv("/home/adrian/Documents/Repos/Evapotranspiration/venv", required = TRUE)
+reticulate::repl_python()
+
+# python packages
+import urllib.request
+import pandas as pd
+import numpy as np
+import datetime
+import xarray as xr
+from joblib import Parallel, delayed
+
+# (FAO56) Penman-Monteith (PM) equation for FT
+PM_function = urllib.request.urlopen("https://git.io/J3Iy5")
+exec(PM_function.read())
+
+# range time for computation
+range_time = pd.date_range("1981-01-01", "1981-01-10", freq="d")
+
+# path files
+file_path_grids = "data/processed/gridded/"
+
+# PM function
+def apply_PM_function(time_step):
+  
+  Time_i = int(time_step.strftime('%j'))
+  Tmax_i = xr.open_dataset(file_path_grids + "sub_variables/values/tmax/tmax_" + time_step.strftime('%Y-%m-%d') + ".nc").layer
+  Tmin_i = xr.open_dataset(file_path_grids + "sub_variables/values/tmin/tmin_" + time_step.strftime('%Y-%m-%d') + ".nc").layer
+  Sd_i = xr.open_dataset(file_path_grids + "sub_variables/values/sd/sd_" + time_step.strftime('%Y-%m-%d') + ".nc").layer
+  Td_i = xr.open_dataset(file_path_grids + "sub_variables/values/td/td_" + time_step.strftime('%Y-%m-%d') + ".nc").layer
+  Ws_m = xr.open_dataset(file_path_grids + "sub_variables/normals/ws/ws_" + time_step.strftime('%m') + ".nc").layer
+  LAT = xr.open_dataset(file_path_grids + "co_variables/Y.nc").Y
+  Z = xr.open_dataset(file_path_grids + "co_variables/DEM.nc").DEM
+  
+  pm_eo_evap = penman_monteith_FAO56_FT(time_i = Time_i,
+                                        tmax_i = Tmax_i.values,
+                                        tmin_i = Tmin_i.values,
+                                        sd_i = Sd_i.values,
+                                        td_i = Td_i.values,
+                                        u2_i = Ws_m.values,
+                                        lat_i = LAT.values,
+                                        z_i = Z.values)
+  
+  to_save = xr.open_dataset(file_path_grids + "co_variables/DEM.nc")
+  to_save["eo"] = (('latitude', 'longitude'), pm_eo_evap)
+  to_save["eo"].to_netcdf(file_path_grids + "eo/eo_" + time_step.strftime('%Y-%m-%d') + ".nc")
+
+
+# applying PM function
+Parallel(n_jobs=2, verbose=50)(
+  delayed(apply_PM_function)(i) for i in range_time
+  )  
